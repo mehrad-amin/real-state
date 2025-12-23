694479460829001ac265385637af0461ce4e0857
Mehrad aminsharei <mehradaminsharei@gmail.com>
2025-12-23 20:11:19 +0330

    fix

--- src/app/buy-residential/page.js
+++ src/app/buy-residential/page.js
@@ -4,9 +4,12 @@ import React from "react";
 const BuyResidentials = async ({ searchParams }) => {
   const params = await searchParams;
 
-  const res = await fetch("http://localhost:3000/api/profile", {
-    cache: "no-store",
-  });
+  const res = await fetch(
+    "https://real-state-mu-umber.vercel.app/api/profile",
+    {
+      cache: "no-store",
+    }
+  );
   const data = await res.json();
   if (data.error) return <h3>مشکلی به وجود امده است </h3>;
   let finalData = data.data;
